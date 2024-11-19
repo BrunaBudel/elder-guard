71689ec4765ce8a1c2e29980aea89e114e3698ab
BrunaBudel <bruna.budel@houden.com.br>
2024-11-19 11:50:51 -0300

    feat: install zod

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -27,9 +27,7 @@ export default function AuthLayout({
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Homepage</a></li>
-              <li><a>Portfolio</a></li>
-              <li><a>About</a></li>
+              <li><a href="/formularios">Formulários</a></li>
             </ul>
           </div>
         </div>
